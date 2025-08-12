add4d6eff5d64cd5d9a5e27f6e3157dda93eeec8
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-08-12 09:48:23 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/springtopicos20252/Springtopicos20252Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springtopicos20252;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtopicos20252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtopicos20252Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springtopicos20252/Springtopicos20252ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springtopicos20252;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtopicos20252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
