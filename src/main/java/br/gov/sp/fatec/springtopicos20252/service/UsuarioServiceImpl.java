961f2a487f84a66b89b8acee436dad816829461f
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-10-06 17:38:52 +0000

    Spring Security

--- src/main/java/br/gov/sp/fatec/springtopicos20252/service/UsuarioServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springtopicos20252/service/UsuarioServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,13 +24,18 @@ public class UsuarioServiceImpl implements UsuarioService{
 
     private AutorizacaoRepository autRepo;
 
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepo, AutorizacaoRepository autRepo) {
+    private PasswordEncoder encoder;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepo, 
+            AutorizacaoRepository autRepo, PasswordEncoder encoder) {
         this.repo = usuarioRepo;
         this.autRepo = autRepo;
+        this.encoder = encoder;
     }
 
     @Override
     @Transactional
+    @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     public Usuario novo(Usuario usuario) {
         if(usuario == null ||
                 usuario.getNome() == null ||
@@ -38,6 +45,7 @@ public class UsuarioServiceImpl implements UsuarioService{
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Faltam informações!");
         }
         usuario.setId(null);
+        usuario.setSenha(encoder.encode(usuario.getSenha()));
         Set<Autorizacao> autorizacoes = new HashSet<Autorizacao>();
         for(Autorizacao aut: usuario.getAutorizacoes()) {
             autorizacoes.add(buscarAutorizacaoPorId(aut.getId()));
@@ -61,6 +69,7 @@ public class UsuarioServiceImpl implements UsuarioService{
     }
 
     @Override
+    @PreAuthorize("isAuthenticated()")
     public Usuario buscarPeloId(Long id) {
         Optional<Usuario> usuarioOp = repo.findById(id);
         if(usuarioOp.isEmpty()) {
